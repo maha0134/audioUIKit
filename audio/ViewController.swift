04f09f6804d3257fc782b10ccd7071371f170d34
Akshay Mahajan <maha0134@algonquinlive.com>
2023-06-25 18:26:18 +0530

    add audio and audioPlayback

--- audio/ViewController.swift
+++ audio/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+	
+	var audioPlayer: AVAudioPlayer?
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 
-
+	@IBAction func playButtonTapped(_ sender: UIButton) {
+		print("button clicked!")
+		//Check if the audio exists
+		guard let audioPath = Bundle.main.url(forResource: "sampleAudio", withExtension: "mp3") else {
+			print("Failed to find the audio file")
+			return
+		}
+		//Audio has been found
+		print("Audio found!")
+		do {
+			audioPlayer = try AVAudioPlayer(contentsOf: audioPath)
+			audioPlayer?.play()
+		} catch {
+			print("couldn't play audio")
+		}
+	}
+	
 }
 
